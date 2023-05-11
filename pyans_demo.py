fefc027cb0570b41c41cdd14f252bfe5bc027fa0
Oliver Lindemann <lindemann09@users.noreply.github.com>
2023-05-11 19:39:46 +0200

    Update pyans_demo.py

--- pyans_demo.py
+++ pyans_demo.py
@@ -3,7 +3,7 @@ from pyans import AssignmentDB, load_db
 
 database_name = "fswp_21-22.ansdb"
 
-# setup new database, if it does not yet, otherwise loaded existing
+# setup new database, if it does not exist, otherwise load
 if not os.path.isfile(database_name):
     db = AssignmentDB()
     db.initialize(start_date="1.9.2021", end_date="31.8.2022",
@@ -14,7 +14,7 @@ else:
 
 print(db.overview())
 
-# retrieve missing data (downloaded exercises/submissions takes a while)
+# retrieve missing data (download exercises/submissions takes a while)
 db.retrieve(results=True, exercises=False, submissions=False)
 
 # show courses
@@ -22,5 +22,5 @@ print(db.course_list_df().to_string())
 
 # export data
 data = db.grades_df() # = db.assignments_df()
-# since data is a Pandas dataframe you can export
+# since data is a Pandas dataframe you can export to different file formats (see Pandas documentation)
 data.to_csv("myexport.csv")
